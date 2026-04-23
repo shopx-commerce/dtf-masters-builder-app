be3f262c336e92781750bbae8e107757206f2fd6
asma-asif-brainx <asma.asif@brainxtech.com>
2026-04-23 18:04:32 +0500

    add close button on toast

--- client/src/components/ui/toast.tsx
+++ client/src/components/ui/toast.tsx
@@ -77,7 +77,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      "absolute right-2 top-2 rounded-sm p-1.5 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 group-[.warning]:text-amber-700 group-[.warning]:hover:text-amber-900",
+      "absolute right-2 top-2 rounded-sm p-1.5 text-foreground/50 opacity-100 transition-colors hover:text-foreground focus:outline-none focus:ring-2 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 group-[.warning]:text-amber-700 group-[.warning]:hover:text-amber-900",
       className
     )}
     toast-close=""
