49f179efd74335e3ebd0482c7ea2acf2ec509808
asma-asif-brainx <asma.asif@brainxtech.com>
2026-07-23 15:44:25 +0500

    Add loading screen on add to cart and update design

--- client/src/lib/translations/en.ts
+++ client/src/lib/translations/en.ts
@@ -42,6 +42,8 @@ export const en: Record<string, string> = {
   "editor.addDesignTitle": "Add another image",
   "editor.processing": "Processing...",
   "editor.processingDesign": "Processing your design",
+  "editor.addingToCartModal": "Adding to Cart...",
+  "editor.updatingDesignModal": "Updating your design...",
   "editor.optimizing": "Optimizing for the best print quality...",
   "editor.autoArrange": "Auto-Arrange",
   "editor.autoArrangeAll": "Auto-arrange all designs on gangsheet",
